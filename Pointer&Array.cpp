fd24af578753c26f81e7acc42c0e2744618dbb68
masecebb <167249720+masecebb@users.noreply.github.com>
2025-10-01 03:21:56 +0700

    Update Pointer&Array.cpp

--- Pointer&Array.cpp
+++ Pointer&Array.cpp
@@ -1,55 +1,53 @@
 #include <stdio.h>
 
 int main(){
-	
 	//Array 
+	int num[5] = {1,2,3,4,5};
+	printf("%d\n", num[1]);
+	scanf("%d", &num[1]);
+	for(int i = 0; i < 5; i++){
+		printf("\n%d ", num[i]);
+	}
 	
-//	int num[5] = {1,2,3,4,5};
-//	printf("%d\n", num[1]);
-//	scanf("%d", &num[1]);
-//	for(int i = 0; i < 5; i++){
-//		printf("\n%d ", num[i]);
-//	}
+	char huruf[10] = {'B','R','Y','A','N'};
+	printf("%c\n", huruf[4]);
 	
-//	char huruf[10] = {'B','R','Y','A','N'};
-//	printf("%c\n", huruf[4]);
+	char string[5][20] = {"BECE", "HANCIL", "WEWEY", "PEPIN", "WILCAN"};
+	printf("%s\n", string[1]);
+	scanf("%s", string[1]);
+	for(int i = 0; i < 5; i++){
+		printf("\n%s ", string[i]);
+	}
 	
-//	char string[5][20] = {"BECE", "HANCIL", "WEWEY", "PEPIN", "WILCAN"};
-//	printf("%s\n", string[1]);
-//	scanf("%s", string[1]);
-//	for(int i = 0; i < 5; i++){
-//		printf("\n%s ", string[i]);
-//	}
-	
-//	printf("%c", string[0][3]);
+	printf("%c", string[0][3]);
 
 	//2D Array
 	int matrix[5][5] = {{1,2,3},
 						{1,3,5}};
 						
-//	printf("%d", matrix[0][1]);
+	printf("%d", matrix[0][1]);
 	
-//	for(int i = 0; i < 2; i++){
-//		for(int j = 0; j < 3; j++){
-//			printf("%d ", matrix[i][j]);
-//		}
-//		printf("\n");
-//	}
+	for(int i = 0; i < 2; i++){
+		for(int j = 0; j < 3; j++){
+			printf("%d ", matrix[i][j]);
+		}
+		printf("\n");
+	}
 	
 	//Pointer
-//	int a = 8;
-//	int *ptr = &a;
-//	ptr = &a;
+	int a = 8;
+	int *ptr = &a;
+	ptr = &a;
 	
-//	printf("%d\n", a);
-//	printf("%d\n", *ptr);
+	printf("%d\n", a);
+	printf("%d\n", *ptr);
 
-//	printf("%d\n", &a);
-//	printf("%d\n", ptr);
+	printf("%d\n", &a);
+	printf("%d\n", ptr);
 	
-//	*ptr = 10;
-//	printf("%d\n", a);
-//	printf("%d\n", *ptr);
+	*ptr = 10;
+	printf("%d\n", a);
+	printf("%d\n", *ptr);
 	
 	char huruf = 'O';
 	char *ptr = &huruf;
@@ -61,14 +59,6 @@ int main(){
 	printf("%c\n", huruf);
 	printf("%c\n", *ptr);
 	printf("%c\n", **ptrr);
-
-	
-	
-
-
-	
-	
-	
 	
 	return 0;
 }
